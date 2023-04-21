989d11d39ee41b3a1eb6354786e0d401b2b1d084
Jose Luis <wildchamo@gmail.com>
2023-04-20 21:12:25 -0500

    llamamiento a apis con async await y fetch

---
+++ main.js
@@ -0,0 +1,25 @@
+const api = "https://api.thecatapi.com/v1/images/search?limit=3";
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
+const button = document.querySelector("button");
+
+reload();
+
+async function reload() {
+  const res = await fetch(api);
+  const data = await res.json();
+
+  console.log(data);
+
+  img1.src = data[0].url;
+  img2.src = data[1].url;
+  img3.src = data[2].url;
+
+  
+
+  //  .then((res) => res.json())
+  //     .then((data) => {
+  //
+  //     });
+}
