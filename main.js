44a7b076c57c070827c19d405dbeb9a653daed88
Jose Luis <wildchamo@gmail.com>
2023-04-25 20:11:16 -0500

    axios es una locura, se puede destructurar el estado y el contenido!

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+const instance = axios.create({ baseURL: "https://api.thecatapi.com/v1/" });
+instance.defaults.headers.common["X-API-KEY"] =
+  "live_vih61PK0ZQGbXpTRRv8KDCUrC4KXhmGjnKQ63tmplsD3hMAO8r74Kg48LSJJ36EA";
+
 const api_random =
   "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_vih61PK0ZQGbXpTRRv8KDCUrC4KXhmGjnKQ63tmplsD3hMAO8r74Kg48LSJJ36EA";
-const api_favorite =
-  "https://api.thecatapi.com/v1/favourites/";
+const api_favorite = "https://api.thecatapi.com/v1/favourites/";
+const api_submit = "https://api.thecatapi.com/v1/images/upload";
 const api_delete_favorite = (id) =>
-  `https://api.thecatapi.com/v1/favourites/${id}/?&api_key=live_vih61PK0ZQGbXpTRRv8KDCUrC4KXhmGjnKQ63tmplsD3hMAO8r74Kg48LSJJ36EA`;
+  `https://api.thecatapi.com/v1/favourites/${id}`;
 
 const img1 = document.getElementById("img1");
 const img2 = document.getElementById("img2");
@@ -19,6 +23,30 @@ const spanError = document.getElementById("error");
 
 loadRandomMichis();
 loadFavoritesMichis();
+
+async function uploadMichi() {
+  const form = document.getElementById("uploadingForm");
+  const formData = new FormData(form);
+
+  console.log(formData.get("file"));
+  const res = await fetch(api_submit, {
+    method: "POST",
+    headers: {
+      "X-API-KEY":
+        "live_vih61PK0ZQGbXpTRRv8KDCUrC4KXhmGjnKQ63tmplsD3hMAO8r74Kg48LSJJ36EA",
+    },
+    body: formData,
+  });
+  const data = await res.json();
+
+  if (res.status !== 201) {
+    spanError.innerHTML = `Hubo un error al subir michi: ${res.status} ${data.message}`;
+  } else {
+    console.log("Foto de michi cargada :)");
+    console.log(data);
+    saveFavoriteMichi(data.id); //para agregar el michi cargado a favoritos.
+  }
+}
 
 async function loadRandomMichis() {
   const res = await fetch(api_random);
@@ -43,11 +71,12 @@ async function loadRandomMichis() {
 }
 
 async function loadFavoritesMichis() {
-  const res = await fetch(api_favorite,{
+  const res = await fetch(api_favorite, {
     method: "GET",
-    headers:{
-      'X-API-KEY': "live_vih61PK0ZQGbXpTRRv8KDCUrC4KXhmGjnKQ63tmplsD3hMAO8r74Kg48LSJJ36EA"
-    }
+    headers: {
+      "X-API-KEY":
+        "live_vih61PK0ZQGbXpTRRv8KDCUrC4KXhmGjnKQ63tmplsD3hMAO8r74Kg48LSJJ36EA",
+    },
   });
   const data = await res.json();
 
@@ -78,26 +107,40 @@ async function loadFavoritesMichis() {
 }
 
 async function saveFavoriteMichi(id) {
-  const res = await fetch(api_favorite, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      image_id: `${id}`,
-    }),
+  const {data,status} = await instance.post("/favourites", {
+    image_id: id,
   });
 
-  if (res.status !== 200) {
-    spanError.innerHTML = "Hubo un error: " + res.status;
+  console.log(data)
+  // const res = await fetch(api_favorite, {
+  //   method: "POST",
+  //   headers: {)
+  //     "Content-Type": "application/json",
+  //     "x-api-key":
+  //       "live_vih61PK0ZQGbXpTRRv8KDCUrC4KXhmGjnKQ63tmplsD3hMAO8r74Kg48LSJJ36EA",
+  //   },
+  //   body: JSON.stringify({
+  //     image_id: `${id}`,
+  //   }),
+  // });
+
+
+
+  if (status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + status;
   } else {
     loadFavoritesMichis();
   }
-
-  console.log(res);
 }
 
 async function deleteFavoriteMichi(id) {
   const res = await fetch(api_delete_favorite(id), {
     method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key":
+        "live_vih61PK0ZQGbXpTRRv8KDCUrC4KXhmGjnKQ63tmplsD3hMAO8r74Kg48LSJJ36EA",
+    },
   });
   if (res.status !== 200) {
     spanError.innerHTML = "Hubo un error: " + res.status;
